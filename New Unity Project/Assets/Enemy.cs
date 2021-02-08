2eb7b4639499444878a2a35a74f49abc64b51170
Fangszx <jygrzn@gmail.com>
2021-02-08 13:47:55 +0800

    created path finding using waypoints

--- New Unity Project/Assets/Enemy.cs
+++ New Unity Project/Assets/Enemy.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //speed of the movement
+    public float speed = 10f;
+
+    //target object
+    private Transform target;
+    private int wavepointIndex = 0;
+
+
+    private void Start()
     {
-        
+        //set the target to the first waypoint
+        target = Waypoint.points[0];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    private void GetNextWayPoint()
+    {
+        if (wavepointIndex >= Waypoint.points.Length-1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointIndex++;
+        target = Waypoint.points[wavepointIndex];
     }
 }
