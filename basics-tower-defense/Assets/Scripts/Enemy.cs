9cfa65722ec773067d71153d01aef95a5ea47a19
Fangszx <jygrzn@gmail.com>
2021-02-11 21:59:26 +0800

    redo the project
    playspace update

---
+++ basics-tower-defense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    //enemy speed
+    public float speed = 10f;
+
+    //movement target
+    private Transform target;
+
+    //starting index of wave
+    private int wavepointIndex = 0;
+
+    //in start method, set the target = the first element in the static array inside Waypoint.cs
+
+    private void Start()
+    {
+        target = Waypoint.points[0];
+    }
+
+    private void Update()
+    {
+        //get the direction
+        Vector3 directionOfMovement = target.position - transform.position;
+        transform.Translate(directionOfMovement.normalized * speed * Time.deltaTime, Space.World);
+    
+        if(Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GoToNextWaypoint();
+
+        }
+    }
+
+    private void GoToNextWaypoint()
+    {
+        if(wavepointIndex >= Waypoint.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointIndex++;
+        target = Waypoint.points[wavepointIndex];
+    }
+
+}
