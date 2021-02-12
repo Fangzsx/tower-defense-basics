d1fda6bf140b464177048706b225fcff7af3da91
Fangszx <jygrzn@gmail.com>
2021-02-12 20:38:56 +0800

    added wave spawner

---
+++ basics-tower-defense/Assets/WaveSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    //object to spawn
+    public Transform enemyPrefab;
+
+    //time between each wave to spawn
+    private float timeBetweenSpawn = 5f;
+
+    //countdown for wave to spawn
+    private float countdown = 0f;
+
+    //number of enemies to spawn per wave
+    private int waveIndex = 0;
+
+    //spawn location
+    public Transform spawnPoint;
+
+    private void Update()
+    {
+        if(countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenSpawn;
+        }
+
+        //decrease the countdown by 1 per frame
+        countdown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+
+        //iterate over the waveNumberIndex
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+        
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+
+}
