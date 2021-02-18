90e0c7f52bf367a084cd0c7472956d552178cacf
Fangszx <jygrzn@gmail.com>
2021-02-18 21:04:38 +0800

    created bullet effect
    note: bug wherein the target does not get destroyed upon hit

---
+++ basics-tower-defense/Assets/Scripts/Bullet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 70f;
+    public GameObject impactEffect;
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+
+    private void Update()
+    {
+        if(target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        //get direction
+        Vector3 direction = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        //if the actual distance <= distance this frame
+        if(direction.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        //move the bullet
+        transform.Translate(direction.normalized * distanceThisFrame , Space.World);
+    }
+    void HitTarget()
+    {
+        GameObject effect = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effect, 2f);
+        Destroy(gameObject);
+    }
+}
--- basics-tower-defense/Assets/Scripts/Turret.cs
+++ basics-tower-defense/Assets/Scripts/Turret.cs
@@ -2,18 +2,36 @@
 
 public class Turret : MonoBehaviour
 {
+
     //reference for target
-    public Transform target;
+    private Transform target;
+
+    [Header("Attributes")]
     //turrent attack range
-    private float range = 15f;
+    public float range = 15f;
+
+    //turnSpeed
+    public float turnSpeed = 10f;
+
+    public float fireRate = 1f;
+    //time between each fire
+    private float fireCountown = 0f;
+
     //tag for enemy
     private string enemyTag = "Enemy";
 
-    //turnSpeed
-    private float turnSpeed = 10f;
+    [Header("Unity Setup fields")]
+    
+    //bullet config params
+
+    //bullet prefab
+    public GameObject bulletPrefab;
 
     //part to rotate
     public Transform partToRotate;
+
+    //firepoint for bullet
+    public Transform firePoint;
 
     private void Start()
     {
@@ -30,8 +48,30 @@ public class Turret : MonoBehaviour
         Quaternion lookDirection = Quaternion.LookRotation(direction);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookDirection, Time.deltaTime * turnSpeed) .eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
-        
 
+
+        //firing
+        if(fireCountown <= 0)
+        {
+            Shoot();
+            fireCountown = 1f / fireRate;
+        }
+
+        fireCountown -= Time.deltaTime;
+        
+    }
+
+    void Shoot()
+    {
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        //make sure that the bullet instance is not empty
+        if(bullet != null)
+        {
+            bullet.Seek(target);
+        }
+        
     }
 
     void UpdateTarget()
