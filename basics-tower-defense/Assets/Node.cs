426819475df5206a888a4fd237a416e2afc4c9a5
Fangszx <jygrzn@gmail.com>
2021-02-25 20:44:32 +0800

    build turret onmousedown

--- basics-tower-defense/Assets/Node.cs
+++ basics-tower-defense/Assets/Node.cs
@@ -5,6 +5,8 @@ public class Node : MonoBehaviour
 {
 
     public Color hoverColor;
+    public GameObject turret;
+
     private Renderer rend;
     private Color startColor;
 
@@ -12,6 +14,21 @@ public class Node : MonoBehaviour
     {   
         rend = GetComponent<Renderer>();
         startColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        if(turret != null)
+        {
+            Debug.Log("Can't build on the current location! TODO: Display on screen");
+            return;
+        }
+
+
+        //build a turret
+        GameObject turretTobuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretTobuild, transform.position, transform.rotation);
+
     }
 
     void OnMouseEnter()
