69b3672b76821b9605b38df2af4d40bd97d890b6
Fangszx <jygrzn@gmail.com>
2021-03-12 21:10:11 +0800

    added another turret
    disable clicking inside the turret icon

--- basics-tower-defense/Assets/Scripts/Node.cs
+++ basics-tower-defense/Assets/Scripts/Node.cs
@@ -1,5 +1,5 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Node : MonoBehaviour
 {
@@ -7,6 +7,7 @@ public class Node : MonoBehaviour
     public Color hoverColor;
     public GameObject turret;
     public Vector3 positionOffset;
+    BuildManager buildManager;
 
     private Renderer rend;
     private Color startColor;
@@ -15,10 +16,18 @@ public class Node : MonoBehaviour
     {   
         rend = GetComponent<Renderer>();
         startColor = rend.material.color;
+        buildManager = BuildManager.instance;
     }
 
     private void OnMouseDown()
     {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
+        if (buildManager.GetTurretToBuild() == null)
+            return;
+
+
         if(turret != null)
         {
             Debug.Log("Can't build on the current location! TODO: Display on screen");
@@ -34,6 +43,11 @@ public class Node : MonoBehaviour
 
     void OnMouseEnter()
     {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
+        if (buildManager.GetTurretToBuild() == null)
+            return;
         rend.material.color = hoverColor;
     }
 
